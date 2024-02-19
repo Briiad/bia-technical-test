eb3f7b49e0a862668b5a9bb61391b62bae11c3ac
Brian Aditya <brianadit33@gmail.com>
2024-02-19 17:22:40 +0700

    feat: add homepage content

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import App from './App.jsx'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import store from './store.js'
+import { Provider } from 'react-redux'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
